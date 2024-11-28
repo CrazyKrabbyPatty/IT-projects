12de5ff76dbde66f37478d1b471678d77af45055
CrazyKrabbyPatty <CrazyKrabbyPatty@gmail.com>
2024-11-29 00:45:00 +0300

    Add files via upload

---
+++ HomeWork №4/Set Storage.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <set>
+
+void set_input(std::set<int> SET, int size) {
+	int number;
+	for(int i = 0; i < size; i++)
+	{	
+		std::cin >> number;
+		SET.insert(number);
+	}
+}
+
+int main() {
+	int N;
+	std::cin >> N;
+	std::set<int> setA{};
+	std::set<int> setB{};
+	set_input(setA*, N);
+	for (int n : setA) {
+		std::cout << n << "\t";
+	}
+	std::cout << std::endl;
+	return 0;
+}
