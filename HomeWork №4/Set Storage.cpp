800a8d80c4f17b921d9438fa234932bd2228ad2e
CrazyKrabbyPatty <CrazyKrabbyPatty@gmail.com>
2024-11-29 16:33:32 +0300

    Add files via upload

--- HomeWork №4/Set Storage.cpp
+++ HomeWork №4/Set Storage.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <set>
 
-void set_input(std::set<int> SET, int size) {
+void set_input(std::set<int>& SET, int size) {
 	int number;
 	for(int i = 0; i < size; i++)
 	{	
@@ -10,15 +10,47 @@ void set_input(std::set<int> SET, int size) {
 	}
 }
 
+std::set<int> numbers_in_both_sets(std::set<int> SET1, std::set<int> SET2, int size) {
+	std::set<int> FSET{};
+	for(const int n : SET1)
+	{
+		if (SET2.find(n) != SET2.end())
+		{
+			FSET.insert(n);
+		}
+	}
+	return FSET;
+}
+
 int main() {
 	int N;
 	std::cin >> N;
+
 	std::set<int> setA{};
 	std::set<int> setB{};
-	set_input(setA*, N);
-	for (int n : setA) {
-		std::cout << n << "\t";
+
+	std::cout << "Введите числа в множество A" << std::endl;
+	set_input(setA, N);
+
+	std::cout << "Введите числа в множество B" << std :: endl;
+	set_input(setB, N);
+
+	std::set<int> IntersectionSet = numbers_in_both_sets(setA, setB, N);
+
+	int t;
+	std::cout << "Введите t" << std::endl;
+	std::cin >> t;
+
+	std::set<int> FSET{};
+	for(const int n : IntersectionSet)
+	{
+		if (n > t)
+		{
+			FSET.insert(n);
+		}
 	}
-	std::cout << std::endl;
+
+	for (int n : FSET)
+		std::cout << n;
 	return 0;
 }
